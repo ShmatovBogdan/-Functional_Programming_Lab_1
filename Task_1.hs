51dbe5b61c6312c34654343fc784c57f93220223
ShmatovBogdan <bogdanshmatow2@gmail.com>
2025-09-06 09:28:41 +0300

    Update Task_1.hs

--- Task_1.hs
+++ Task_1.hs
@@ -1,3 +1,5 @@
+-- Написати функцію doubleEven, яка для списку цілих чисел подвоює тільки парні числа, залишаючи непарні без змін.
+
 doubleEven :: [Int] -> [Int]
 doubleEven xs = [if even x then x * 2 else x | x <- xs]
 
